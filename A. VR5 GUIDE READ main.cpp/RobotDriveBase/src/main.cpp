d82053dd49a899a4b8f7792693414ee6163547b8
Giovanni Bernal <you@example.com>
2019-09-27 01:42:25 -0700

    VEX Code

---
+++ A. VR5 GUIDE READ main.cpp/RobotDriveBase/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\super                                            */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+brain       Brain;
+
+// define your global instances of motors and other devices here
+
+
+int main() {
+    
+}
