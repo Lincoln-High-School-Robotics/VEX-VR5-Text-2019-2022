90cdb419b44cc2eba5daaa4a8bda50357f9fe3e8
Giovanni Bernal <you@example.com>
2019-09-27 18:10:00 -0700

    VEX Code

--- RobotDriveByMotor/src/main.cpp
+++ RobotDriveByMotor/src/main.cpp
@@ -2,10 +2,11 @@
 
 //Abreviations:       Kw: Keyword    VarN: Variable Name     Parm: Parameters
 
+//  This code controls the Drive Base in which each Josytick (there are 2) controls One (1) motor
+//  Free: 2 X Axis One (1) from each Joystick
+
 
 #include "vex.h"         // VEX Library, Always include
-
-
 using namespace vex;     
 
 
@@ -14,16 +15,20 @@ using namespace vex;
 brain Brain;                                
 motor rightMotor = motor(PORT1, false);     
 motor leftMotor =  motor(PORT2, false);      
-controller Control;
-
-double LM, RM, Vector;
-
+controller Control;                     // controller VarN     Initializes a variable for the control; No parameters needed
 
 
 int main(){
 
 
-while(true){
+  //To access the Joystick we use controllerVarN.AxisNumber.value/position()
+  //where:
+  // Axis1 = Right Joystick X Axis
+  // Axis2 = Right Joystick Y Axis
+  // Axis3 = Left  Joystick Y Axis
+  // Axis4 = Left  Joystick X Axis
+
+while(true){                                                                     //Repeats forever
 
 
    leftMotor.setVelocity(Control.Axis2.position(), velocityUnits::pct);
